5d57d32a14be626510d3095a4ccd41562ca90629
Jokertwo <p.lastovka@seznam.cz>
2018-04-24 16:54:10 +0200

    add command textfield

--- src/main/java/com/aveco/Gstreamer/Main.java
+++ src/main/java/com/aveco/Gstreamer/Main.java
@@ -11,6 +11,7 @@ import com.aveco.Gstreamer.ctrl.ITestControler;
 import com.aveco.Gstreamer.ctrl.IVideoPlayerCtrl;
 import com.aveco.Gstreamer.ctrl.TestControler;
 import com.aveco.Gstreamer.ctrl.VideoPlayerCtrl;
+import com.aveco.Gstreamer.gui.CommandTextField;
 import com.aveco.Gstreamer.gui.IMyGVideoPlayer;
 import com.aveco.Gstreamer.gui.LogInfo;
 import com.aveco.Gstreamer.gui.MyGVideoPlayer;
@@ -20,8 +21,7 @@ import com.aveco.Gstreamer.log.CreateLogger;
 
 public class Main {
 
-    private static final String PATH = "\\com\\aveco\\res\\V309750.mp4";
-    
+    public static final String PATH = "\\com\\aveco\\res\\a.mp4";
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
@@ -34,15 +34,18 @@ public class Main {
         logger.trace("Inicialize of Gsreamer");
         args = Gst.init("FirstAppGst", args);
 
+        CommandBuffer commandBuffer = new CommandBuffer();
+
         IMyGVideoPlayer videoPlayer = new MyGVideoPlayer(getURI());
         ITestControler ctrlTest = new TestControler(videoPlayer);
         IVideoPlayerCtrl ctrlVP = new VideoPlayerCtrl(videoPlayer, ctrlTest);
 
         // start gui
-        SwingUtilities.invokeLater(() -> new MyGWindow(videoPlayer.getPanel(), logInfo));
+        SwingUtilities
+            .invokeLater(() -> new MyGWindow(videoPlayer.getPanel(), logInfo, new CommandTextField(commandBuffer)));
 
         // start command line
-        new Thread(new CommandLine(ctrlVP)).start();
+        new Thread(new CommandLine(ctrlVP, commandBuffer)).start();
 
     }
 
@@ -60,6 +63,18 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
+//        installShutdownHook();
         new Main(args);
+    }
+
+
+    private static void installShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
+            System.out.println("ShotDown GStreamer");
+
+            Gst.deinit();
+
+        }, "Shutdown-thread"));
     }
 }
--- src/main/java/com/aveco/Gstreamer/gui/MyGVideoPlayer.java
+++ src/main/java/com/aveco/Gstreamer/gui/MyGVideoPlayer.java
@@ -2,16 +2,17 @@ package com.aveco.Gstreamer.gui;
 
 import java.awt.BorderLayout;
 import java.net.URI;
+import java.util.List;
 import javax.swing.JPanel;
+import org.freedesktop.gstreamer.Bus;
 import org.freedesktop.gstreamer.Bus.EOS;
 import org.freedesktop.gstreamer.Bus.ERROR;
-import org.freedesktop.gstreamer.Event;
+import org.freedesktop.gstreamer.Bus.STATE_CHANGED;
+import org.freedesktop.gstreamer.Bus.TAG;
 import org.freedesktop.gstreamer.Gst;
 import org.freedesktop.gstreamer.GstObject;
-import org.freedesktop.gstreamer.Pad;
-import org.freedesktop.gstreamer.Pad.EVENT_PROBE;
-import org.freedesktop.gstreamer.PadProbeReturn;
 import org.freedesktop.gstreamer.State;
+import org.freedesktop.gstreamer.TagList;
 import org.freedesktop.gstreamer.elements.PlayBin;
 import org.freedesktop.gstreamer.examples.SimpleVideoComponent;
 import org.slf4j.Logger;
@@ -26,6 +27,8 @@ public class MyGVideoPlayer extends JPanel implements IMyGVideoPlayer {
     private SimpleVideoComponent vCmp;
     private PlayBin playBin;
 
+    private Bus.TAG tag;
+
 
     public MyGVideoPlayer(URI uri) {
 
@@ -37,37 +40,67 @@ public class MyGVideoPlayer extends JPanel implements IMyGVideoPlayer {
             vCmp = new SimpleVideoComponent();
             logger.trace("SimpleVideoComponent was created");
 
+            addListeners();
+
             playBin.setVideoSink(vCmp.getElement());
             playBin.setURI(uri);
-            
+
             setLayout(new BorderLayout());
             add(vCmp, BorderLayout.CENTER);
 
-
-            playBin.getBus().connect(new EOS() {
-                @Override
-                public void endOfStream(GstObject source) {
-                    playBin.pause();
-                    logger.debug("End of video -> video was paused");
-                }
-            });
-            logger.trace("EOS listener was add to playBin");
-
-            playBin.getBus().connect(new ERROR() {
-
-                @Override
-                public void errorMessage(GstObject source, int code, String message) {
-                    System.out.println("GstObject: " + source);
-                    System.out.println("Error code: " + code);
-                    System.out.println("Message: " + message);
-                }
-            });
-            logger.trace("ERROR listener was add to playBin");
-
             playBin.setState(State.PAUSED);
+
         } else {
             logger.error("GStreamer is not inicialized");
         }
+    }
+
+
+    private void addListeners() {
+        tag = new TAG() {
+
+            @Override
+            public void tagsFound(GstObject source, TagList tagList) {
+                List<String> names = tagList.getTagNames();
+
+                for (String name : names) {
+                    System.out.println(name + " : " + tagList.getValue(name, 0));
+
+                }
+
+            }
+
+        };
+        playBin.getBus().connect(tag);
+
+        playBin.getBus().connect(new STATE_CHANGED() {
+
+            @Override
+            public void stateChanged(GstObject source, State old, State current, State pending) {
+                System.out.println(old + "\t" + current + "\t" + pending);
+
+            }
+        });
+
+        playBin.getBus().connect(new EOS() {
+            @Override
+            public void endOfStream(GstObject source) {
+                playBin.pause();
+                logger.debug("End of video -> video was paused");
+            }
+        });
+        logger.trace("EOS listener was add to playBin");
+
+        playBin.getBus().connect(new ERROR() {
+
+            @Override
+            public void errorMessage(GstObject source, int code, String message) {
+                System.out.println("GstObject: " + source);
+                System.out.println("Error code: " + code);
+                System.out.println("Message: " + message);
+            }
+        });
+        logger.trace("ERROR listener was add to playBin");
     }
 
 
