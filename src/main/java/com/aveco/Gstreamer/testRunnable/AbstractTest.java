9245c4d5244e1b45d6384a9523f77bb73a1d0763
Jokertwo <p.lastovka@seznam.cz>
2018-04-24 09:36:24 +0200

    Add new test

--- src/main/java/com/aveco/Gstreamer/testRunnable/AbstractTest.java
+++ src/main/java/com/aveco/Gstreamer/testRunnable/AbstractTest.java
@@ -1,13 +1,18 @@
 package com.aveco.Gstreamer.testRunnable;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
+import org.apache.commons.io.FileUtils;
+import org.freedesktop.gstreamer.elements.PlayBin;
+import com.aveco.Gstreamer.ctrl.ITestControler;
+import org.slf4j.Logger;
+
 
 public abstract class AbstractTest implements Runnable {
 
@@ -15,36 +20,86 @@ public abstract class AbstractTest implements Runnable {
     private boolean run = true;
     private int yPosition = 23;
 
+    private ITestControler tCtrl;
+    private PlayBin playBin;
+    private JComponent panel;
+
+
+    public ITestControler gettCtrl() {
+        return tCtrl;
+    }
+
+
+    public PlayBin getPlayBin() {
+        return playBin;
+    }
+
+
+    public JComponent getPanel() {
+        return panel;
+    }
+
+
+    public AbstractTest(ITestControler tCtrl, PlayBin playBin, JComponent panel) {
+        super();
+        this.tCtrl = tCtrl;
+        this.playBin = playBin;
+        this.panel = panel;
+    }
+
     private Object lock = new Object();
     private int counter = 0;
+
 
     public void stopTest() {
         synchronized (lock) {
             run = false;
         }
     }
-    
-    public void saveImage(String folderName,String testName,JComponent panel,List<Pair> values){
+
+
+    public void saveImage(String folderName, String testName, JComponent panel, List<Pair> values) {
         int i = 1;
+        
         BufferedImage bi = new BufferedImage(panel.getSize().width * 2 + 50, panel.getSize().height,
             BufferedImage.TYPE_INT_ARGB);
         Graphics g = bi.createGraphics();
-        panel.paintComponents(g);
-        
+        panel.paint(g);
+
         g.setFont(new Font(Font.MONOSPACED, Font.BOLD, 25));
-        for(Pair item : values){
+        for (Pair item : values) {
             g.setColor(item.getColor());
             g.drawString(item.getValue(), bi.getWidth() / 2 + 10, yPosition * i);
             i++;
         }
         g.dispose();
-        
+
         try {
-            ImageIO.write(bi, "png", new File(folderName + "/" + testName + String.valueOf(counter) + ".png"));
+            ImageIO.write(bi, "png", new File(PATH + folderName + "/" + testName + String.valueOf(counter) + ".png"));
             counter++;
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    public void prepare(String folder, Logger logger) {
+        logger.info("Deleting the result of the previous test if exist and prepare new empty folder");
+        try {
+            if (new File(PATH + folder).exists()) {
+                FileUtils.deleteDirectory(new File(PATH + folder));
+                createFile(PATH + folder);
+            } else {
+                createFile(PATH + folder);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void createFile(String folder) {
+        new File(folder).mkdirs();
     }
 
 
---
+++ src/main/java/com/aveco/Gstreamer/testRunnable/FrameStepAccuracy.java
@@ -0,0 +1,53 @@
+package com.aveco.Gstreamer.testRunnable;
+
+import java.awt.Color;
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.JComponent;
+import org.freedesktop.gstreamer.Gst;
+import org.freedesktop.gstreamer.elements.PlayBin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.aveco.Gstreamer.ctrl.ITestControler;
+
+public class FrameStepAccuracy extends AbstractTest {
+
+    private static final String FRAME_STEP = "frameStepTest";
+    public static final Logger logger = LoggerFactory.getLogger(FrameStepAccuracy.class);
+    
+    public FrameStepAccuracy(ITestControler tCtrl, PlayBin playBin, JComponent panel) {
+        super(tCtrl, playBin, panel);
+    }
+
+
+
+
+    @Override
+    public void run() {
+        logger.info("Start of test 'FrameStepAccuracy'");
+        Thread.currentThread().setName("Test-FrameStepAccuracy");
+        prepare(FRAME_STEP, logger);
+        
+        getPlayBin().pause();
+        logger.info("Video was paused");
+        
+        while (isRunnig()) {
+            sleep(200);
+            gettCtrl().stepForward(1);
+            sleep(200);
+            saveImage(FRAME_STEP, FrameStepAccuracy.class.getName(), getPanel(), getValues());
+        }
+        getPlayBin().pause();
+        logger.info("End of test 'FrameAccuracy'");
+
+    }
+    
+    
+    private List<Pair> getValues() {
+        List<Pair> values = new LinkedList<Pair>();
+        values.add(new Pair(gettCtrl().getActualFrame(), Color.BLACK));
+        values.add(new Pair(gettCtrl().presentationTimeStemp().toString(),Color.BLACK));
+        return values;
+    }
+
+}
